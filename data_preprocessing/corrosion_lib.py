9fe31ad004d417a1eea276b8a7b5de5e0216feca
James Wong <jmswong12@gmail.com>
2022-11-28 12:36:43 -0800

    Update preprocessing to allow unordered inputs

--- data_preprocessing/corrosion_lib.py
+++ data_preprocessing/corrosion_lib.py
@@ -7,9 +7,14 @@ import zipfile
 NUM_TIMESTEPS_PER_SIMULATION = 9
 
 # Unzip specified number of corrosion files into the same directory,
-# ignoring any edge files.
-def extract_corrosion_output(path, num_simulations = 1):
-  corrosion_filenames = get_corrosion_filenames(num_simulations)
+# ignoring any edge files. If extract_all is true, ignore num_simulations
+# and extract every file in the specified path.
+def extract_corrosion_output(path, num_simulations = 1, extract_all = False):
+  import pdb; pdb.set_trace()
+  if extract_all:
+    corrosion_filenames = get_all_filenames_from_zip(path)
+  else:
+    corrosion_filenames = get_corrosion_filenames(num_simulations)
 
   corrosion_path_lst = path.split('/')
   corrosion_dir_base = '/'.join(corrosion_path_lst[:-1])
@@ -38,6 +43,43 @@ def get_corrosion_filenames(num_simulations = 1):
       filenames.append(filename)
   return filenames
 
+# Similar to get_corrosion_filenames, but takes in a list of
+# (simulation, timestep) tuples and only extracts the specified samples.
+def get_corrosion_filenames_subset(simulation_timesteps):
+  filenames = []
+  for simulation_idx, timestep in simulation_timesteps:
+    filename = "Corrosion_simulation_%d_timeStep_%d.txt" % (simulation_idx, timestep)
+    filenames.append(filename)
+  return filenames
+
+# Returns a list of corrosion simulation filenames which match any output file
+# from the given corrosion.zip path.
+def get_all_filenames_from_zip(path):
+  filenames = []
+  with zipfile.ZipFile(path, 'r') as zip_obj:
+    filenames = zip_obj.namelist()
+
+  # filter out MACOSX
+  filenames = [file for file in filenames if "MACOSX" not in file]
+
+  # filter out directories
+  filenames = [file for file in filenames if file[-1] != '/']
+
+  # extract simulation id and timestep
+  simulation_timesteps = []
+  for filepath in filenames:
+    filename = filepath.split("/")[1]
+    matches = re.match(r'output_(\d+)_(\d+).mat', filename)
+    simulation_idx = int(matches.group(1))
+    timestep = int(matches.group(2))
+    simulation_timesteps.append((simulation_idx, timestep))
+
+  # construct corrosion filenames
+  return get_corrosion_filenames_subset(simulation_timesteps)
+
+# Returns a list of corrosion simulation filenames, for the first num_simulation
+# datapoints. Note that the files are 1-indexed, so this returns filenames from
+
 # Returns a 1d corrosion map given a single filepath. A 1d corrosion map is
 # represented as a python dictionary, with keys representing the location on the
 # x-axis along a horizontal rebar, and the values representing corrosion depth
@@ -58,11 +100,16 @@ def extract_1d_corrosion_map_from_filepath(filepath):
 
 # Returns a list of pairs, each containing the filename of the simulation, and
 # a 1-d corrosion map.
-def extract_1d_corrosion_maps(output_dir, num_simulations = 1):
+def extract_1d_corrosion_maps(output_dir, num_simulations = 1, simulation_timesteps = None):
   corrosion_dir = output_dir + "/corrosion"
 
+  if simulation_timesteps is not None:
+    corrosion_filenames = get_corrosion_filenames_subset(simulation_timesteps)
+  else:
+    corrosion_filenames = get_corrosion_filenames(num_simulations)
+
   file_and_corrosion_map = []
-  for filename in get_corrosion_filenames(num_simulations):
+  for filename in corrosion_filenames:
     filepath = corrosion_dir + '/' + filename
     corrosion_map = extract_1d_corrosion_map_from_filepath(filepath)
     file_and_corrosion_map.append((filepath, corrosion_map))
