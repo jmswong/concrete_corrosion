a2ceb857b53d1902a21dbbb9473266e42dd010a1
James Wong <jmswong12@gmail.com>
2022-12-02 17:13:18 -0800

    Style

--- data_preprocessing/data_normalization.py
+++ data_preprocessing/data_normalization.py
@@ -11,15 +11,23 @@ args = parser.parse_args()
 
 def normalize_data(training_features, test_features):
     '''
-	Normalize corrosion depth and concrete property features on training set. Apply the same normalization to the test set. This is stable so for inference, this function should be called with the same training data, and the inference dataset.
+	Normalize corrosion depth and concrete property features on training set.
+    Apply the same normalization to the test set. This is stable so for
+    inference, this function should be called with the same training data,
+    and the inference dataset.
 
     Args:
-		training_features: Numpy array of shape (num_train_samples, 343). Each row contains: simulation_idx, timestep, 4 concrete property featurs, and 337 corrosion depth features.
-		test_features: Numpy array of shape (num_test_samples, 343). Same as training_features.
+		training_features: Numpy array of shape (num_train_samples, 343).
+            Each row contains: simulation_idx, timestep, 4 concrete property
+            featurs, and 337 corrosion depth features.
+		test_features: Numpy array of shape (num_test_samples, 343). Same
+            as training_features.
 
 	Returns:
-		training_features_normalized: Numpy array of the same shape as training_features, with normalization.
-		test_features_normalized: Numpy array of the same shape as test_features, with normalization.
+		training_features_normalized: Numpy array of the same shape as
+            training_features, with normalization.
+		test_features_normalized: Numpy array of the same shape as
+            test_features, with normalization.
     '''
     return training_features, test_features
 
